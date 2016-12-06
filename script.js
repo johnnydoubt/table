66001e1f7d98dd88d1a1ed95d53c10170de8567b
johnnydoubt <cesar.tailleur@gmail.com>
2016-12-06 14:18:08 +0100

    tableau chargé depuis le JS ok

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const ready = function() {
+    const people = [
+    {firstname: "Roberto", surname: "Dupuis"},
+    {firstname: "Julia", surname: "Dupont"},
+    {firstname: "Ezekiel", surname: "Duparc"},
+    {firstname: "Buddy", surname: "Dupet"}
+    ];
+    const tbody = document.querySelector("tbody");
+
+for (let i =0;i<people.length;i++) {
+
+    let tr = document.createElement("tr");
+  //  let tdp = document.createElement("td");
+  //  let tdn = document.createElement("td");
+ tr.innerHTML = `<td>${people[i].firstname}</td><td>${people[i].surname}</td>`;
+
+  //  let firstname = document.createTextNode(people[i].firstname);
+  //  let surname = document.createTextNode(people[i].surname);
+  //  tdn.appendChild(surname);
+  //  tdp.appendChild(firstname);
+  //  tr.appendChild(tdp);
+  //  tr.appendChild(tdn);
+    tbody.appendChild(tr);
+}}
+
+document.addEventListener("DOMContentLoaded", ready);
