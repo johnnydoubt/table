f9ed772eed4101c4b23f4d11f38c52d5d81a3d4e
johnnydoubt <cesar.tailleur@gmail.com>
2016-12-12 09:48:03 +0100

    modif appendChild pr documentFragment

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-
 const ready = function() {
-    const people = [
-    {firstname: "Roberto", surname: "Dupuis"},
-    {firstname: "Julia", surname: "Dupont"},
-    {firstname: "Ezekiel", surname: "Duparc"},
-    {firstname: "Buddy", surname: "Dupet"}
-    ];
+    const people = [{
+        firstname: "Roberto",
+        surname: "Dupuis"
+    }, {
+        firstname: "Julia",
+        surname: "Dupont"
+    }, {
+        firstname: "Ezekiel",
+        surname: "Duparc"
+    }, {
+        firstname: "Buddy",
+        surname: "Dupet"
+    }];
     const tbody = document.querySelector("tbody");
 
-for (let i =0;i<people.length;i++) {
+    let trcreate = document.createDocumentFragment();
 
-    let tr = document.createElement("tr");
-  //  let tdp = document.createElement("td");
-  //  let tdn = document.createElement("td");
- tr.innerHTML = `<td>${people[i].firstname}</td><td>${people[i].surname}</td>`;
+    for (let i = 0; i < people.length; i++) {
 
-  //  let firstname = document.createTextNode(people[i].firstname);
-  //  let surname = document.createTextNode(people[i].surname);
-  //  tdn.appendChild(surname);
-  //  tdp.appendChild(firstname);
-  //  tr.appendChild(tdp);
-  //  tr.appendChild(tdn);
-    tbody.appendChild(tr);
-}}
+        let tr = document.createElement("tr");
+        //  let tdp = document.createElement("td");
+        //  let tdn = document.createElement("td");
+        tr.innerHTML = `<td>${people[i].firstname}</td><td>${people[i].surname}</td>`;
+
+        //  let firstname = document.createTextNode(people[i].firstname);
+        //  let surname = document.createTextNode(people[i].surname);
+        //  tdn.appendChild(surname);
+        //  tdp.appendChild(firstname);
+        //  tr.appendChild(tdp);
+        //  tr.appendChild(tdn);
+        trcreate.appendChild(tr);
+
+    }
+    tbody.appendChild(trcreate);
+}
 
 document.addEventListener("DOMContentLoaded", ready);
